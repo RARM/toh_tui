a8af0e8e1cbd2e66f3626d174224662900d0b46a
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2022-03-29 23:27:57 -0400

    Started working on the Academic handler.

--- src/academic_handler.h
+++ src/academic_handler.h
@@ -10,17 +10,34 @@ class Academic_Handler
 {
 public:
 	// Setup the game.
-	// Academic_Handler();
+	Academic_Handler();
 
-	// Display current game state (disks in rods).
+	// Display current game state (disks in rods) and prompt for next roung (or game over).
 	// void display();
 
-	// The main function can make direct calls to the ToH_Game object.
-	// friend void run_academic();
+	// Make move using ToH_Game constants.
+	// int try_move(int src, int dst);
 
 private:
+	// screen calculations
+	static constexpr size_t screen_height{ 4 }; // how tall the screen is going to be
+	static constexpr size_t tower_width{ 5 }; // the width occupied by the towers
+	static constexpr size_t id_spacing{ 2 }; // width of the ids representing the towers on the left
+	static constexpr size_t ft_spacing{ 6 }; // total space between towers after id_spacing is added (full tower spacing)
+	static constexpr size_t screen_width{ (tower_width + id_spacing) * 3 + ft_spacing * 2 }; // newline not included
+
+	// members
 	ToH_Game game;
 	size_t moves;
+	char screen[screen_height][screen_width];
+
+	// Helper functions.
+
+	// Update the screen content for display using current state.
+	// void update_screen();
+
+	// Remove any characters from the rods that represent the disks.
+	// void clear_screen();
 };
 
 #endif
--- src/academic_hanlder.cpp
+++ src/academic_hanlder.cpp
@@ -2,12 +2,26 @@
 
 /*
 * About Academic Mode
-* This mode does not use ncurses. It
+* This mode does not use ncurses. The game works like the example
+* given by the professor in the start document of the journey folder.
 */
+#include <iostream>
 #include "academic_handler.h"
 
 void run_academic() {
 
 
     return;
+}
+
+Academic_Handler::Academic_Handler()
+    : game{}, moves{ 0 } {
+    // Initialize screen.
+    for (size_t i{ 0 }; i < Academic_Handler::screen_height; i++)
+        for (size_t j{ 0 }; j < Academic_Handler::screen_width; j++) screen[i][j] = ' ';
+
+    // Letter A, B, and C are in the base of each rod.
+    screen[Academic_Handler::screen_height - 1][Academic_Handler::id_spacing + (Academic_Handler::tower_width / 2) + 1] = 'A';
+    screen[Academic_Handler::screen_height - 1][Academic_Handler::ft_spacing + Academic_Handler::id_spacing * 2 + Academic_Handler::tower_width + (Academic_Handler::tower_width / 2) + 1] = 'B';
+    screen[Academic_Handler::screen_height - 1][Academic_Handler::ft_spacing * 2 + Academic_Handler::id_spacing * 3 + Academic_Handler::tower_width * 2 + (Academic_Handler::tower_width / 2) + 1] = 'C';
 }
