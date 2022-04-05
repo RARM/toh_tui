e8ab811fe65da67ec68f81c77779c4a01ac5b1e9
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2022-04-04 22:54:57 -0400

    Added a menu when the game is first started.
    Changed the main handler to display a menu when the game is launched.
    This allows the user to select which version of the game play.

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,38 @@
 int main(void) {
 
 #if FULL_VERSION
-    char in;
-    // Ask for the version to run.
-    std::cout << "Select the version to run: ";
-    std::cin >> in;
+    char user_input;
+    bool valid_input;
 
-    if (in == '1') run_full();
-    else run_academic();
+    std::cout << "Copyright (c) 2022 Rodolfo Andres Rivas Matta.\n"
+              << "Welcome to the Tower of Hanoi Game!\n\n";
+    
+    do
+    {
+        std::cout << "==============================================\n"
+                  << "Menu:\n"
+                  << "1. Academic version (no TUI).\n"
+                  << "2. TUI version (using ncurses).\n"
+                  << "3. Exit.\n"
+                  << "Enter an option: " << std::flush;
+        
+        std::cin >> user_input;
+        valid_input = user_input == '1' || user_input == '2' || user_input == '3';
+        if(!valid_input) std::cout << "Invalid input. Please, try again.\n\n";
+    } while (!valid_input);
+
+    switch (user_input)
+    {
+    case '1':
+        std::cout << "==============================================\n\n";
+        run_academic();
+        break;
+
+    case '2':
+        run_full();
+        break;
+    }
+    
 #else
     run_academic();
 #endif
